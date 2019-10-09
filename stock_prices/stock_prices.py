4746cf0dd7ee6caa1b4a307be7c63244275ed0fc
rushi444 <rushi44@gmail.com>
2019-10-09 13:33:14 -0700

    Stock prices finished

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,24 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max = prices[1]
+  least = prices[0]
+  max_index = 0
+  if len(prices) < 2:
+    return print("Not enough data")
+  for i in range(2, len(prices) - 1):
+    if prices[i] > max:
+      max = prices[i]
+      max_index = i
+  for e in range(1, max_index):
+    if prices[e] < least:
+      least = prices[e]
+  return max - least
+  
+find_max_profit([1050, 270, 1540, 3800, 2])
+
+
+
 
 
 if __name__ == '__main__':
