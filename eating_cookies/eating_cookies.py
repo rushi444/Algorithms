989c4b13ea425c3b1cf075cad2e121ee4ef3ad54
rushi444 <rushi44@gmail.com>
2019-10-10 06:24:45 -0700

    first two done, stuck on third

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,27 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+counter = 0
+
 def eating_cookies(n, cache=None):
-  pass
+  global counter
+  if n <= 0:
+    None
+    # N = number of cookies
+    # 3 ways to subtract a cookie
+    # Each time we subtract a cookie (meaning he ate the cookie(s)), we increase the counter += 1
+    #n = 4
+  else:
+    for i in range(1, 4):
+      if n > 0:
+        print("before n", n)
+        n = n - i
+        counter += 1
+        print("n", n)
+        eating_cookies(n)
+  return counter
+
+print(eating_cookies(4))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  if(len(recipe) != len(ingredients)):
+    return 0
+  array = []
+  for i in recipe and ingredients:
+    array.append(ingredients[i] // recipe[i])
+  return min(array)
+  
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 })
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
